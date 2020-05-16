00ec7ad19a418cf584803e1eee0b9c20b695cc0e
prasenjitghose36 <prasenjitghose36@gmail.com>
2020-05-17 01:29:17 +0530

    first push on may 17 jai jagannath

---
+++ check _server.py
@@ -0,0 +1,5 @@
+import socket 
+import threading
+
+SERVER = socket.gethostbyname(socket.gethostname())
+print(SERVER)
---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.1.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello World!")
+send("Hello Everyone!")
+send("Hello I am Prasenjit")
+
+send(DISCONNECT_MESSAGE)
